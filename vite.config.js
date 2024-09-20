48cb278dc397b574cc52d0edce29f09ab786ec4e
Vishesh Tomer <vishesh@scriza.in>
2024-09-20 18:55:08 +0530

    fytuy

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-paginate', 'yup'], // Add yup here along with react-paginate
+      external: [] // Add yup here along with react-paginate
     },
   },
 })
